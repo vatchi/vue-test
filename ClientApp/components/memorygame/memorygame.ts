f076f254396f76ca729027499031f0e8ef964b3d
vatchi <love.hermansson@live.se>
2018-04-11 13:12:56 +0200

    init

---
+++ ClientApp/components/memorygame/memorygame.ts
@@ -0,0 +1,19 @@
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class MemoryGameComponent extends Vue {
+    currentcount: number = 0;
+    numberOfPictures: number = 24;
+    memoryCards: string = '';
+
+    incrementCounter() {
+        this.currentcount++;
+    }
+
+    showPictures() {
+        for (let i = 0; i < this.numberOfPictures; i++) {
+            this.memoryCards += '<div class="memoryCard col-md-1"></div>';
+        }
+    }
+}
